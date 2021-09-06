6be63f2c11ddfc65851c1cf5115f9794996a76a7
furryalt <87579289+furryalt@users.noreply.github.com>
2021-09-06 16:29:17 +0100

    Add files via upload

--- mercury.py
+++ mercury.py
@@ -1,4 +1,5 @@
-
+sortvar1 = "\n"
+sortvar2 = "\\n"
 #input string name
 textdir = input("text name: \n")
 
@@ -17,7 +18,11 @@ bible=bible.lower()
 biblesort1 = str(bible).replace(",", "")
 biblesort2 = str(biblesort1).replace("'", "")
 biblesort3 = str(biblesort2).replace("(", "")
-bible = str(biblesort3).replace(")", "")
+biblesort4 = str(biblesort3).replace(sortvar2, " ")
+biblesort5 = str(biblesort4).replace(sortvar1, "")
+biblesort6 = str(biblesort5).replace("\\", "")
+biblesort7 = str(biblesort6).replace('"', "")
+bible = str(biblesort7).replace(")", "")
 
     #sorts words
 bible = bible.split()
@@ -33,18 +38,15 @@ biblesort3 = str(biblesort2).replace("(", "")
 biblesort5 = str(biblesort3).replace(")", "")
 biblesort6 = str(biblesort5).replace("", "")
 biblesort7 = str(biblesort6).replace("[", "")
-biblesort8 = str(biblesort7).replace("]", "")
-
-
-#remove duplicates
+bibleend = str(biblesort7).replace("]", "")
 
 
 #output varible
-final = biblesort8
+final = bibleend
 
 print(final) 
 #code from the internet im using to output a file
 
 f = open( 'final.txt', 'w' )
-f.write(repr(biblesort8))
+f.write(repr(bibleend))
 f.close()
