c1a2fb5ded6ddb03352850e2c920cca3c3b1b2cd
furryalt <87579289+furryalt@users.noreply.github.com>
2021-09-10 20:18:40 +0100

    Add files via upload

--- run.py
+++ run.py
@@ -1,24 +1,33 @@
 import lyricsgenius as genius
+import PySimpleGUI as sg
+artistent = [[sg.Text("input artist's name")], [sg.Input(key = "artist_name")], [sg.Text("amount of songs")], [sg.Input(key = "max_songs")], [sg.Button("enter")]]
 
+
+window = sg.Window('hello', artistent)
 count=0
 
 #api key
 geniusly = genius.Genius("xB4UsUST9Wx-Ns3emec3xOkK-GhFsWmwFH7tWNBbQjvJrR7nPpYYgPnEcEyFNLWF")
-
-#get varibles used for searching songs
-artist_name = input("artist name\n")
-number = input("how many songs\n")
-number = int(number)
 api = genius.Genius("xB4UsUST9Wx-Ns3emec3xOkK-GhFsWmwFH7tWNBbQjvJrR7nPpYYgPnEcEyFNLWF")
 
+#creates GUI window for getting artist name
+event, values = window.read()
+    
+#   get varibles used for searching songs
+#artist_name = input("artist name\n")
+#number = input("how many songs\n")
+#number = int(number)
+number = values["max_songs"]
+max_songs = values["max_songs"]
+artist_name = values["artist_name"]
+
+max_songs = int(max_songs)
 #search song
-artist = api.search_artist(artist_name, max_songs=number)
+artist = api.search_artist(artist_name, max_songs)
 
 #integrate old sort into this file
 #puts songs into text files
 for song in artist.songs:
-    sortvar1 = "\n"
-    sortvar2 = "\\n"
     #input string name
     bible = song.lyrics
     count = int(count) +1
@@ -29,8 +38,8 @@ for song in artist.songs:
     biblesort1 = str(bible).replace(",", "")
     biblesort2 = str(biblesort1).replace("?", "")
     biblesort3 = str(biblesort2).replace("(", " ")
-    biblesort4 = str(biblesort3).replace(sortvar2, " ")
-    biblesort5 = str(biblesort4).replace(sortvar1, " ")
+    biblesort4 = str(biblesort3).replace("\n", " ")
+    biblesort5 = str(biblesort4).replace("\\n", " ")
     biblesort6 = str(biblesort5).replace("\\", " ")
     biblesort7 = str(biblesort6).replace('"', " ")
     biblesort8 = str(biblesort7).replace(")", " ")
