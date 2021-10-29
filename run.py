29407f9e08e822d0579def82d399fdbbb39c91f7
furryalt <87579289+furryalt@users.noreply.github.com>
2021-10-30 00:47:04 +0100

    Add files via upload

--- run.py
+++ run.py
@@ -1,76 +1,68 @@
 import lyricsgenius as genius
 import PySimpleGUI as sg
-artistent = [[sg.Text("input artist's name")], [sg.Input(key = "artist_name")], [sg.Text("amount of songs")], [sg.Input(key = "max_songs")], [sg.Button("enter")]]
-
-
-window = sg.Window('hello', artistent)
-count=0
-
-#api key
-geniusly = genius.Genius("xB4UsUST9Wx-Ns3emec3xOkK-GhFsWmwFH7tWNBbQjvJrR7nPpYYgPnEcEyFNLWF")
+windowcfg = [[sg.Text("input artist's name:")], [sg.Input(key = "artist_name")], [sg.Text("amount of songs:")], [sg.Input(key = "max_songs")], [sg.Checkbox("single output file?", default=True, key="single")], [sg.Checkbox("sort text?", default=True, key="sort")], [sg.Button("enter")]]
+window = sg.Window("mercury UwU", windowcfg)
 api = genius.Genius("xB4UsUST9Wx-Ns3emec3xOkK-GhFsWmwFH7tWNBbQjvJrR7nPpYYgPnEcEyFNLWF")
-
-#creates GUI window for getting artist name
 event, values = window.read()
-    
-#   get varibles used for searching songs
-#artist_name = input("artist name\n")
-#number = input("how many songs\n")
-#number = int(number)
-number = values["max_songs"]
-max_songs = values["max_songs"]
+finala = ""
+count=0
+  
+#retrive varibles from gui used for searching songs
+max_songs = int(values["max_songs"])
 artist_name = values["artist_name"]
+individual_out = values["single"]
+sort = values["sort"]
 
-max_songs = int(max_songs)
 #search song
 artist = api.search_artist(artist_name, max_songs)
 
-#integrate old sort into this file
-#puts songs into text files
 for song in artist.songs:
-    #input string name
     bible = song.lyrics
     count = int(count) +1
     print(count)
     #makes words lower case
     bible=bible.lower()
     #cleans input words
-    biblesort1 = str(bible).replace(",", "")
-    biblesort2 = str(biblesort1).replace("?", "")
-    biblesort3 = str(biblesort2).replace("(", " ")
+    biblesort3 = str(bible).replace("(", " ")
     biblesort4 = str(biblesort3).replace("\n", " ")
     biblesort5 = str(biblesort4).replace("\\n", " ")
     biblesort6 = str(biblesort5).replace("\\", " ")
     biblesort7 = str(biblesort6).replace('"', " ")
-    biblesort8 = str(biblesort7).replace(")", " ")
-    biblesort9 = str(biblesort8).replace("[", " ")
-    biblesort10 = str(biblesort9).replace("]", " ")
-    biblesort11 = str(biblesort10).replace("-", " ")
-    bible = str(biblesort11).replace(")", "")
+    biblesort8 = str(biblesort7).replace(")", "")
+    biblesort9 = str(biblesort8).replace("[", "")
+    biblesort10 = str(biblesort9).replace("]", "")
+    bibleprepared = str(biblesort10).replace("-", " ")
     
-    #sorts words
-    bible = bible.split()
-    bibletext = bible
-    bibletext.sort()
-    biblesort = str(bibletext)
-    bibletext.sort()
-    
-    #cleans output words
-    biblesort1 = str(bibletext).replace(",", "")
-    biblesort2 = str(biblesort1).replace("'", "")
-    biblesort3 = str(biblesort2).replace("(", "")
-    biblesort5 = str(biblesort3).replace(")", "")
-    biblesort6 = str(biblesort5).replace('"', "")
-    biblesort7 = str(biblesort6).replace("[", "")
-    bibleend = str(biblesort7).replace("]", "")
-    
-    
-    #output varible
+    if sort:
+        #sorts words
+        sorter = bibleprepared.split()
+        sorter.sort()
+        bibletext = str(sorter)
+        biblesort1 = str(bibletext).replace(",", "")
+        biblesort2 = str(biblesort1).replace("'", "")
+        biblesort3 = str(biblesort2).replace("(", "")
+        biblesort5 = str(biblesort3).replace(")", "")
+        biblesort6 = str(biblesort5).replace('"', "")
+        biblesort7 = str(biblesort6).replace("[", "")
+        biblesort8 = str(biblesort7).replace("?", "")
+        biblesort9 = str(biblesort8).replace(":", "")
+        bibleend = str(biblesort9).replace("]", "")
+    else:
+        bibleend = bibleprepared
     final = bibleend
-
-    weewoo = "final" + str(count) + ".txt"
-    #code from the internet im using to output a file
     
-    f = open("./output/" + weewoo, 'w')
-    f.write(repr(bibleend))
-    f.close()
+    if individual_out:
+        #NOTE TO MY FORGETFULL FUTURE SELF: dumb workaround logic for the api because of the way it handles storing each song
+        #but basically, finala = finala(at start is blank) + final(current song stored in sorting system), if you ask for 3 songs
+        #lyricsgenius will load in one song, sort it and move on to the next song.... meaning the current sorted song is wiped from memory. 
+        #this function accumulates it into one permenant list. DONT REMOVE.
+        finala = finala + final
+        weewoo = "final.txt"
+        f = open("./output/" + weewoo, 'w')
+        f.write(repr(finala))
+        f.close()
+    else:
+        weewoo = "final" + str(count) + ".txt"
+        f = open("./output/" + weewoo, 'w')
+        f.write(repr(bibleend))
+        f.close()
